e9c2d29ad462349902b2b11e2faa435be4c41731
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2025-04-10 02:06:35 +0200

    Fix nan issue

--- src/tone_mapping.py
+++ src/tone_mapping.py
@@ -10,7 +10,6 @@ import torchmetrics
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import lpips
 import cv2
 import lightning as L
 from lightning.pytorch.loggers import TensorBoardLogger
@@ -28,19 +27,19 @@ def read_exr(im_path: str) -> ndarray:
 def generate_exposures(hdr_img, target_val=1.2):
     max_val = np.max(hdr_img)
     median_val = np.median(hdr_img)
-    
+
     c_start = np.log(target_val / max_val) / np.log(2.0)
     c_end   = np.log(target_val / median_val) / np.log(2.0)
-    
+
     exp_values = [c_start, (c_start + c_end) / 2.0, c_end]
-    
+
     exposures = []
     for exp in exp_values:
         scaling_factor = np.sqrt(2.0) ** exp
         scaled_img = hdr_img * scaling_factor
         exposure_img = np.clip(scaled_img, 0, 1)
         exposures.append(exposure_img)
-    
+
     return exposures
 
 def tone_map_mantiuk(image: ndarray) -> ndarray:
@@ -81,52 +80,78 @@ class VGG19Extractor(nn.Module):
 class NoisyDataset(Dataset):
     def __init__(self, root_folder, transform=None):
         self.transform = transform
-
-        self.images_low = []
-        self.images_mid = []
-        self.images_high = []
-        self.images_ulaw = []
-        self.images_original = []
-        
-        for fname in os.listdir(root_folder):
-            image_src = cv2.cvtColor(read_exr(root_folder + '/' + fname), cv2.COLOR_BGR2RGB)
-            image_hdr = 0.5 * image_src / np.mean(image_src)
-            i_hdr = np.median(image_hdr)
-            u = 8.759 * i_hdr**2.148 + 0.1494 * i_hdr**(-2.067)
-
-            # plt.imshow(image_hdr)
-            # plt.show()
-            images = generate_exposures(image_hdr)
-            self.images_low.append(images[0])
-            # plt.imshow(self.images_low[-1])
-            # plt.show()
-            self.images_mid.append(images[1])
-            # plt.imshow(self.images_mid[-1])
-            # plt.show()
-            self.images_high.append(images[2])
-            # plt.imshow(self.images_high[-1])
-            # plt.show()
-            self.images_ulaw.append(np.log10(1.0 + u * image_hdr) / np.log10(1.0 + u))
-            self.images_original.append(image_hdr)
-
+        self.root_folder = root_folder
+        self.images = os.listdir(root_folder)
+        self.transform = transform
 
     def __len__(self):
-        return len(self.images_low)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        img_low = self.images_low[idx]
-        img_mid = self.images_mid[idx]
-        img_high = self.images_high[idx]
-        img_ulaw = self.images_ulaw[idx]
-        img_original = self.images_original[idx]
-        if self.transform:
-            img_low = self.transform(img_low)
-            img_mid = self.transform(img_mid)
-            img_high = self.transform(img_high)
-            img_ulaw = self.transform(img_ulaw)
-            img_original = self.transform(img_original)
+        fname = self.images[idx]
+        image_src = cv2.cvtColor(read_exr(self.root_folder + '/' + fname), cv2.COLOR_BGR2RGB)
+        image_hdr = 0.5 * image_src / np.mean(image_src)
+        i_hdr = np.median(image_hdr)
+        u = 8.759 * i_hdr**2.148 + 0.1494 * i_hdr**(-2.067)
 
-        return img_low, img_mid, img_high, img_ulaw, img_original
+        images = generate_exposures(image_hdr)
+        image_low = images[0]
+        image_mid = images[1]
+        image_high = images[2]
+        image_ulaw = np.log10(1.0 + u * image_hdr) / np.log10(1.0 + u)
+        images_original = image_hdr
+
+        if self.transform:
+            image_low = self.transform(image_low)
+            image_mid = self.transform(image_mid)
+            image_high = self.transform(image_high)
+            image_ulaw = self.transform(image_ulaw)
+            images_original = self.transform(images_original)
+
+        return image_low, image_mid, image_high, image_ulaw, images_original
+
+
+def normalize_gaussian(feature_image, kernel_size, sigma, device):
+    interval = (2 * sigma + 1.) / kernel_size
+    space = np.linspace(-sigma - interval / 2., sigma + interval / 2., kernel_size + 1)
+    kern1d = np.diff(norm.cdf(space))
+
+    kernel = np.sqrt(np.outer(kern1d, kern1d))
+    kernel /= kernel.sum()
+    kernel = kernel.astype(np.float32)
+    kernel_tensor = torch.from_numpy(kernel).unsqueeze(0).unsqueeze(0).repeat(feature_image.shape[1], 1, 1, 1).detach().to(device)
+
+    blurred_image = torch.nn.functional.conv2d(feature_image, kernel_tensor, padding='same', groups=feature_image.shape[1]).detach()
+
+    return (feature_image - blurred_image) / (torch.abs(blurred_image) + EPSILON)
+
+def calculate_local_mean(input_img, kernel_size, device):
+    mean_filter = torch.ones(input_img.shape[1], 1, kernel_size, kernel_size).to(device)
+    padding = kernel_size // 2
+    # img_ms = torch.sign(input_img) * torch.abs(input_img)**(0.5 if u_law_processing else 1.0)
+    mean = torch.nn.functional.conv2d(input_img, mean_filter / kernel_size ** 2, padding=padding, groups=input_img.shape[1]).detach()
+    std = (torch.nn.functional.conv2d(input_img ** 2, mean_filter/ kernel_size ** 2, padding=padding, groups=input_img.shape[1]) - mean ** 2).detach()
+    img_mn = torch.sqrt(torch.abs(std)) / (torch.abs(mean) + EPSILON)
+    return img_mn
+
+def calculate_feature_contrast_masking(gauss_out, mean_out, u_law_processing):
+    img_ms = torch.sign(gauss_out) * torch.abs(gauss_out)**(0.5 if u_law_processing else 1.0)
+    return img_ms / (1.0 + mean_out)
+
+def compute_luminance(img):
+    r, g, b = img[0], img[1], img[2]
+    lum = 0.2126 * r + 0.7152 * g + 0.0722 * b  # Standard luminance formula
+    return lum
+
+def restore_color_from_luminance(gt_img, pred_lum, a=0.6):
+    r, g, b = gt_img[0], gt_img[1], gt_img[2]
+    y_gt_lum = compute_luminance(gt_img) + EPSILON
+
+    restored = torch.zeros_like(gt_img)
+    restored[0] = (r / y_gt_lum)**a * pred_lum
+    restored[1] = (g / y_gt_lum)**a * pred_lum
+    restored[2] = (b / y_gt_lum)**a * pred_lum
+    return restored
 
 
 class Encoder(nn.Module):
@@ -145,7 +170,7 @@ class Encoder(nn.Module):
     def forward(self, x):
         return self.encoder_block(x)
 
-    
+
 class Fusion(nn.Module):
     def __init__(self, in_channels=64):
         super(Fusion, self).__init__()
@@ -179,13 +204,13 @@ class Decoder(nn.Module):
         return self.output_layer(decoder_out + img_low + img_mid + img_high)
 
 
-class ToneMappingModel(nn.Module): 
+class ToneMappingModel(nn.Module):
     def __init__(self):
         super(ToneMappingModel, self).__init__()
         self.encoder = Encoder()
         self.fusion = Fusion()
         self.decoder = Decoder()
-        
+
     def forward(self, img_low, img_mid, img_high):
         low_encoded = self.encoder(img_low)
         mid_encoded = self.encoder(img_mid)
@@ -204,52 +229,9 @@ class TMAP(L.LightningModule):
         self.automatic_optimization = False
         self.i = 0
 
-    def normalize_gaussian(self, feature_image, kernel_size, sigma):
-        interval = (2 * sigma + 1.) / kernel_size
-        space = np.linspace(-sigma - interval / 2., sigma + interval / 2., kernel_size + 1)
-        kern1d = np.diff(norm.cdf(space))
-
-        kernel = np.sqrt(np.outer(kern1d, kern1d))
-        kernel /= kernel.sum()
-        kernel = kernel.astype(np.float32)
-        kernel_tensor = torch.from_numpy(kernel).unsqueeze(0).unsqueeze(0).repeat(feature_image.shape[1], 1, 1, 1).detach().to(self.device)
-    
-        blurred_image = torch.nn.functional.conv2d(feature_image, kernel_tensor, padding='same', groups=feature_image.shape[1]).detach()
-        
-        return (feature_image - blurred_image) / (torch.abs(blurred_image) + EPSILON)
-    
-    def calculate_local_mean(self, input_img, kernel_size):
-        mean_filter = torch.ones(input_img.shape[1], 1, kernel_size, kernel_size).to(self.device)
-        padding = kernel_size // 2
-        # img_ms = torch.sign(input_img) * torch.abs(input_img)**(0.5 if u_law_processing else 1.0)
-        mean = torch.nn.functional.conv2d(input_img, mean_filter / kernel_size ** 2, padding=padding, groups=input_img.shape[1]).detach()
-        std = (torch.nn.functional.conv2d(input_img ** 2, mean_filter/ kernel_size ** 2, padding=padding, groups=input_img.shape[1]) - mean ** 2).detach()
-        img_mn = torch.sqrt(torch.abs(std)) / (torch.abs(mean) + EPSILON)
-        return img_mn
-
-    def calculate_feature_contrast_masking(self, gauss_out, mean_out, u_law_processing):
-        img_ms = torch.sign(gauss_out) * torch.abs(gauss_out)**(0.5 if u_law_processing else 1.0)
-        return img_ms / (1.0 + mean_out)
-    
-    def compute_luminance(self, img):
-        r, g, b = img[0], img[1], img[2]
-        lum = 0.2126 * r + 0.7152 * g + 0.0722 * b  # Standard luminance formula
-        return lum
-    
-    def restore_color_from_luminance(self, gt_img, pred_lum, a=0.6):
-        r, g, b = gt_img[0], gt_img[1], gt_img[2]
-        y_gt_lum = self.compute_luminance(gt_img) + EPSILON
-
-        restored = torch.zeros_like(gt_img)
-        restored[0] = (r / y_gt_lum)**a * pred_lum
-        restored[1] = (g / y_gt_lum)**a * pred_lum
-        restored[2] = (b / y_gt_lum)**a * pred_lum
-        return restored
-
     def training_step(self, batch, batch_idx):
         optimizer = self.optimizers()
         optimizer.zero_grad()
-
         img_low, img_mid, img_high, img_ulaw, img_original = batch
         prediction = self.model(img_low, img_mid, img_high)
         # print(torch.min(prediction), torch.max(prediction))
@@ -262,12 +244,12 @@ class TMAP(L.LightningModule):
         #     self.i += 1
         loss = 0
         for feature_prediction, feature_ulaw in zip(self.vgg(prediction), self.vgg(img_ulaw)):
-            prediction_gauss = self.normalize_gaussian(feature_prediction, kernel_size=13, sigma=2)
-            prediction_means = self.calculate_local_mean(feature_prediction, kernel_size=13)
-            feature_prediction_mask = self.calculate_feature_contrast_masking(prediction_gauss, prediction_means, u_law_processing=False)
-            ulaw_gauss = self.normalize_gaussian(feature_ulaw, kernel_size=13, sigma=2)
-            ulaw_means = self.calculate_local_mean(feature_ulaw, kernel_size=13)
-            feature_ulaw_mask = self.calculate_feature_contrast_masking(ulaw_gauss, ulaw_means, u_law_processing=True)
+            prediction_gauss = normalize_gaussian(feature_prediction, kernel_size=13, sigma=2, device=self.device)
+            prediction_means = calculate_local_mean(feature_prediction, kernel_size=13, device=self.device)
+            feature_prediction_mask = calculate_feature_contrast_masking(prediction_gauss, prediction_means, u_law_processing=False)
+            ulaw_gauss = normalize_gaussian(feature_ulaw, kernel_size=13, sigma=2, device=self.device)
+            ulaw_means = calculate_local_mean(feature_ulaw, kernel_size=13, device=self.device)
+            feature_ulaw_mask = calculate_feature_contrast_masking(ulaw_gauss, ulaw_means, u_law_processing=True)
             loss += self.l1_loss(feature_prediction_mask, feature_ulaw_mask) / len(self.feature_layers)
         self.manual_backward(loss)
         optimizer.step()
@@ -276,14 +258,14 @@ class TMAP(L.LightningModule):
             pred = prediction[0]
             gt = img_original[0]
 
-            pred_lum = self.compute_luminance(pred)
-            restored_img = self.restore_color_from_luminance(gt, pred_lum)
+            pred_lum = compute_luminance(pred)
+            restored_img = restore_color_from_luminance(gt, pred_lum)
             if self.i % 10 == 0:
-                img = restored_img / 100.0 #torch.max(restored_img)
+                img = restored_img / torch.max(restored_img)
                 print(restored_img.max(), restored_img.min())
-                # img_min = img.min()
-                # img_max = img.max()
-                # img = (img - img_min) / (img_max - img_min + EPSILON)
+                img_min = img.min()
+                img_max = img.max()
+                img = (img - img_min) / (img_max - img_min + EPSILON)
                 plt.imshow(img.cpu().permute(1, 2, 0).numpy())
                 plt.show()
             self.i += 1
@@ -307,11 +289,10 @@ class TMAP(L.LightningModule):
 def train():
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Resize((256, 256)),
     ])
-
     dataset = NoisyDataset(root_folder="./data/output_dir", transform=transform)
-    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
     model = TMAP()
     logger = TensorBoardLogger("models/tmap/tb_logs", "tmap")
     ckpt_save_dir = "models/tmap/"
